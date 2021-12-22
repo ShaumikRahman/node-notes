f82e2818039a1ba403d0329888ca3641b4b3e1c1
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-12-22 18:47:13 +0000

    changed to static folder, styling

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const path = require('path');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, 'pages', 'about.html'));
-});
+// app.get('/', (req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.setHeader('Content-Type', 'text/html');
+//     res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
