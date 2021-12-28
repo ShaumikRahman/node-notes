b31e6a0608361cecd5822fda2e336f4b69c39dc1
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-12-28 19:56:13 +0000

    add new note WIP

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,34 @@
-
-const notes = document.getElementById('notes');
+const notes = document.getElementById("notes");
+const form = document.getElementById("form");
 
 window.onload = () => {
-    fetch('/', {
-        method: 'post'
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        //notes.innerHTML += `<p>${data.text}</p>`;
-        data.forEach(note => {
-            notes.innerHTML += `<p>Note ${note.id}, ${note.title}, created at ${new Date(note.created * 1000)}, ${note.body}</p>`;
-        });
+  fetch("/", {
+    method: "post",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((note) => {
+        notes.innerHTML += `<p>Note ${note.id}, ${
+          note.title
+        }, created at ${new Date(note.created * 1000)}, ${note.body}</p>`;
+      });
     });
-    
-}
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const note = e.target.note.value.trim().replace(/\s\s+/g, " ");
+
+  console.log(note);
+
+  fetch("/add", {
+    method: "POST",
+    body: JSON.stringify({ note }),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
--- server.js
+++ server.js
@@ -5,17 +5,44 @@ const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 app.post('/', async (req,res) => {
-    res.setHeader('Content-type', 'application/json');
+     res.setHeader('Content-type', 'application/json');
 
     fs.readFile(path.join(__dirname, 'notes', 'notes.json'), 'utf-8', (err, data) => {
         if (err) {
             console.log(err);
             res.end();
         } else {
-            console.log(data);
-            res.send(data);
+                res.send(data);
+        }
+    });
+})
+
+app.post('/add', async (req,res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    console.log(req.body.note);
+
+    fs.readFile(path.join(__dirname, 'notes', 'notes.json'), 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end({
+                result: 'fail'
+            });
+        } else {
+            const notes = [...JSON.parse(data), {
+                id: '4',
+                title: 'this is the new note',
+                created: new Date(),
+                modified: "",
+                body: req.body.note
+            }];
+
+            res.send(JSON.stringify(notes));
+
+            // TODO dynamic ID
         }
     });
 })
